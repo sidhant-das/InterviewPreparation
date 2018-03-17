a5f43ddde5adf548e00dc06809505be3a6e648aa
who-isthis <sidhantdas@gmail.com>
2018-03-17 21:11:00 +0530

    Normal changes

--- src/ArrayPractice/BinarySearch.scala
+++ src/ArrayPractice/BinarySearch.scala
@@ -1,14 +1,34 @@
 package ArrayPractice
-
 object BinarySearch extends App {
-
-  def newArrayConstruct: Unit ={
+  newArrayConstruct
+  def newArrayConstruct:Unit  ={
     val arr = Array(2,6,8,15,20,23,26,27)
-    val mid = arr.length/2
-    var newArray = Array() // Difference between Array and Array() in scala
-    for(a <- mid until arr.length){
-      newArray :+ a
+    var left= arr(0)
+    var right = arr(arr.length-1)
+    var mid = arr.length/2
+    var midValue= arr(mid) //20
+    val find = 23
+    while(left <= right) { //20 < 27
+      if (midValue == find){
+        println(mid)
+      return mid
+      }
+      else if (midValue > find) {
+        right = midValue
+        mid = mid/2
+        midValue= arr(mid)
+      }
+
+      else // if (midValue < find)
+      {
+        left = midValue
+        //20
+        mid = mid + (arr.length - mid) / 2
+        midValue = arr(mid)
+
+      }
     }
+
 
   }
 }
