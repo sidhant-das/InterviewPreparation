78214f4e6874defb88ff1541151c1ea84ccca5f3
sidhant.das <sidhantdas@gmail.com>
2019-01-18 19:16:00 +0000

    added code in progress for https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/micro-and-array-update/description/

---
+++ src/main/scala/ArrayPractice/MicroAndArrayUpdate.scala
@@ -0,0 +1,33 @@
+package ArrayPractice
+
+import scala.collection.mutable.ArrayBuffer
+
+object MicroAndArrayUpdate extends App{
+  val noOfTestCase = scala.io.StdIn.readLine().toInt
+  (1 to noOfTestCase).foreach{
+    i =>
+      val testCaseDetail = scala.io.StdIn.readLine()
+      println(testCaseDetail)
+      val inputData = testCaseDetail.split(" ")
+      val arrSize = inputData(0).toInt
+      val highestVal = inputData(1)
+      val actualArr =new ArrayBuffer[Int]() ++ (1 to arrSize).map{
+        _ =>
+          scala.io.StdIn.readLine().toInt
+      }
+      while(){
+        val newarr = actualArr.map(_ + 1)
+      }
+
+
+      newarr.map{
+
+      }
+
+  }
+  def searcharray()={
+
+  }
+
+
+}
